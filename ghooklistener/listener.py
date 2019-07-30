58f4c394f85f7de4f6fe6b041e2660169db1e38d
Achilleas Koutsou <achilleas.k@gmail.com>
2019-07-30 18:18:13 +0200

    [listener] Handle the GH ping
    GH sends a ping to the hook when it's initially set up.  Report this by
    printing to stdout and return OK.

--- ghooklistener/listener.py
+++ ghooklistener/listener.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, Response
+import json
 from typing import Callable, Optional, Dict, Any, Tuple
+from flask import Flask, request, Response
 
 
 PayloadType = Dict[str, Any]
@@ -16,13 +17,45 @@ class Listener(object):
 
     def hook_receive(self):
         # TODO: Handle malformed data and other stuff
-        data = request.data
-        ok, message = self.handlefunc(data)
-        if ok:
-            code = 200
+        data: PayloadType = json.loads(request.data)
+
+        if "hook_id" in data:
+            # Ping event: New hook added
+            ok, message = self._report_new_hook(data)
+            code = 200 if ok else 400
+        elif False:
+            ok, message = self.handlefunc(data)
+            if ok:
+                code = 200
+            else:
+                code = 500
         else:
-            code = 500
+            message = "Unknown payload"
+            code = 400
         return Response(response=message, status=code)
+
+    def _report_new_hook(self, data: PayloadType) -> HandleFuncReturnType:
+        # read keys but return 400 if any expected keys are missing
+        try:
+            zen = data["zen"]
+            hook_id = data["hook_id"]
+
+            hook_config = data["hook"]
+            events = hook_config["events"]
+            hook_url = hook_config["url"]
+            created_at = hook_config["created_at"]
+        except KeyError as ke:
+            key = str(ke.args[0])
+            msg = f"Missing expected key in payload: {key}"
+            print(msg)
+            return False, msg
+
+        print(f"New hook with ID {hook_id} has been set up:")
+        print(f" - Hook URL: {hook_url}")
+        print(f" - Hook events: {', '.join(events)}")
+        print(f" - Creation date: {created_at}")
+        print(f"GH zen: {zen}")
+        return True, "Pong"
 
     def rundev(self, address: Optional[str] = None):
         host: Optional[str] = None
