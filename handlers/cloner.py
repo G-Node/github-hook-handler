959b1022ed2d67b65ba4cb270d0b31f7ba7f10c5
M. Sonntag <michael.p.sonntag@gmail.com>
2019-08-01 18:15:50 +0200

    [cloner] Add supported repositories

--- handlers/cloner.py
+++ handlers/cloner.py
@@ -5,30 +5,40 @@ from ghooklistener import Listener, PayloadType, HandleFuncReturnType
 from os import path
 
 
+GIT_DIR = "/tmp/git_repos"
+SUPPORTED_REPOS = ["odml-terminologies", "odml-templates"]
+
+
 def handlefunc(data: PayloadType) -> HandleFuncReturnType:
     try:
+        repo_name = data["repository"]["name"]
         ref = data["ref"]
         forced = data["forced"]
 
     except (KeyError, TypeError) as unwrap_err:
         return False, f"Invalid payload: {str(unwrap_err)}"
 
+    if repo_name not in SUPPORTED_REPOS:
+        print(f"Unsupported repository {repo_name}")
+        return False, "Pull failed"
+
     if ref != "refs/heads/master":
         print("Not master branch.  Not updating.")
         return True, "OK"
 
     # if it was a force push, we will have to fetch and reset
-    if pull(forced):
+    if pull(repo_name, forced):
         return True, "OK"
 
     return False, "Pull failed"
 
 
-def pull(force: bool) -> bool:
+def pull(repo_name: str, force: bool) -> bool:
     if force:
         print("Force pushes not yet supported")
         print("Doing normal push")
-    cloneloc = "/tmp/place"
+
+    cloneloc = path.join(GIT_DIR, repo_name)
 
     if not path.exists(cloneloc):
         print(f"Invalid git directory: {cloneloc}")
