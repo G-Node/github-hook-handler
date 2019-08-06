7405921d0a80d396a2529490310bc21cc81cfcf8
M. Sonntag <michael.p.sonntag@gmail.com>
2019-08-06 18:28:31 +0200

    [cloner] Fix potential double encode

--- handlers/cloner.py
+++ handlers/cloner.py
@@ -55,12 +55,12 @@ def pull(cloneloc: str, force: bool) -> bool:
     return not p.returncode
 
 
-def create_app(repos_dir, secret_token):
-
+def create_app(repos_dir: str, secret_token: str):
     CONFIG['repos_dir'] = repos_dir
     CONFIG['hosted_repos'] = ["odml-terminologies", "odml-templates"]
 
     print("Setting up listener for cloner handler")
+    secret_token = secret_token.encode('utf-8')
     clonelistener = Listener("cloner", handlefunc=handlefunc, secret_token=secret_token)
 
     return clonelistener
@@ -76,7 +76,7 @@ def main():
 
     repos_dir = sys.argv[1]
     if len(sys.argv) > 2:
-        secret_token = sys.argv[2].encode('utf-8')
+        secret_token = sys.argv[2]
     if len(sys.argv) > 3:
         address = sys.argv[3]
 
